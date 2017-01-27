578caad741b7dd50da7ad335d7986bfa99f0ca05
making <makingx@gmail.com>
2017-01-27 10:30:39 +0900

    Remove Spring Cloud Task

--- src/main/java/com/example/CfSpringBatchDemoApplication.java
+++ src/main/java/com/example/CfSpringBatchDemoApplication.java
@@ -3,10 +3,8 @@ package com.example;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.task.configuration.EnableTask;
 
 @SpringBootApplication
-@EnableTask
 @EnableBatchProcessing
 public class CfSpringBatchDemoApplication {
 
